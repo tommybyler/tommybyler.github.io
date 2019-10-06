db7de0ec0bf3f9197c3d15b5bf41850e54a2577a
synchrxny <55989738+synchrxny@users.noreply.github.com>
2019-10-06 18:40:03 -0400

    assignment 5

---
+++ jsQuiz/question.js
@@ -0,0 +1,13 @@
+class Question {
+   constructor (text, choices, answer){
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+  }
+}
+
+var questions = [
+new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
+new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+new Question("What has not been covered in PUI lab?", ["HTML", "CSS","Javascript", "Java"], "Java"),
+];
