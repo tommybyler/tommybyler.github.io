1151ac030b9928451f7f09590cf86e921263af40
synchrxny <55989738+synchrxny@users.noreply.github.com>
2019-10-07 12:01:12 -0400

    Update question.js

--- jsQuiz/question.js
+++ jsQuiz/question.js
@@ -1,8 +1,11 @@
 class Question {
-   constructor (text, choices, answer){
+  constructor (text, choices, answer){
     this.text = text;
     this.choices = choices;
     this.answer = answer;
+  }
+  isCorrectAnswer(choice){
+    return this.answer === choice;
   }
 }
 
@@ -11,3 +14,20 @@ new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CS
 new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
 new Question("What has not been covered in PUI lab?", ["HTML", "CSS","Javascript", "Java"], "Java"),
 ];
+
+class Quiz {
+  constructor(questions){
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+  }
+	getQuestion() {
+    return this.questions[this.questionIndex];
+	}
+  guess(answer) {
+  if(this.getQuestion().isCorrectAnswer(answer)) {
+   this.score++;
+      }
+  this.questionIndex++;
+  }
+}
