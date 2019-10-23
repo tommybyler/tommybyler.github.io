54734272d09759d8a93d5acca0aaf448ebdf72ab
synchrxny <55989738+synchrxny@users.noreply.github.com>
2019-10-23 18:43:51 -0400

    Update mainfunc.js

--- assignment6a/mainfunc.js
+++ assignment6a/mainfunc.js
@@ -1,10 +1,5 @@
 function highlightButton() {
   var tinyButton = document.getElementById("button1");
-  //var smallButton = document.getElementById("button2");
-  //var medButton = document.getElementById("button3");
-  //var largeButton = document.getElementById("button4");
-
-  //if tinyButton.style.border != 1px
   tinyButton.style.border = "2px solid #E4D5C7";
   tinyButton.style.fontWeight = "700";
   tinyButton.style.boxShadow = "0 3px 8px #dddddd";
