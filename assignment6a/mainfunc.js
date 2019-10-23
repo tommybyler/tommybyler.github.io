b162053514570ec4f0901d050d6e5447d2a67ee6
synchrxny <55989738+synchrxny@users.noreply.github.com>
2019-10-23 19:17:48 -0400

    js change

--- assignment6a/mainfunc.js
+++ assignment6a/mainfunc.js
@@ -1,6 +1,9 @@
 function highlightButton() {
   var tinyButton = document.getElementById("button1");
-  tinyButton.style.border = "2px solid #E4D5C7";
-  tinyButton.style.fontWeight = "700";
-  tinyButton.style.boxShadow = "0 3px 8px #dddddd";
+  element.classList.add("highlightstyle");
+}
+
+function unHighlight() {
+  var tinyButton = document.getElementById("button1");
+  element.classList.remove("highlightstyle");
 }
