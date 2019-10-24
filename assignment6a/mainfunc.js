f3065c92d4dba4a5764081888523a4d274fca6f8
synchrxny <55989738+synchrxny@users.noreply.github.com>
2019-10-24 19:26:49 -0400

    updates

--- assignment6a/mainfunc.js
+++ assignment6a/mainfunc.js
@@ -1,4 +1,5 @@
 var tinyButton = document.getElementById('light');
+
 tinyButton.addEventListener('click', function(e){
        var nodes = tinyButton.children;
        for(var i = 0; i < nodes.length; i++){
@@ -6,6 +7,22 @@ tinyButton.addEventListener('click', function(e){
                nodes[i].className = "";
            }
        }
-       e.target.className = "highlightbutton";
-       console.log(e.target);
+       if(e.target.id === "button1" || e.target.id === "button2" || e.target.id === "button3" || e.target.id === "button4" ){
+           e.target.className = "highlightbutton";
+     }
 });
+
+var cartText = document.getElementById('cartButton');
+var myCartCount = document.getElementById('cartCount');
+var incrementer = 0
+
+function changeCartText(){
+  if(cartText.innerHTML == 'Add to Cart') {
+    cartText.innerHTML = "Added!";
+    incrementer++;
+    myCartCount.innerHTML = incrementer;
+  }
+  else {
+    cartText.innerHTML = "Add to Cart";
+  }
+}
