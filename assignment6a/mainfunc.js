febfbcfa6052acae5be0c9e7487ec803f62a89f7
synchrxny <55989738+synchrxny@users.noreply.github.com>
2019-10-26 15:54:52 -0400

    js updates

--- assignment6a/mainfunc.js
+++ assignment6a/mainfunc.js
@@ -1,6 +1,7 @@
 var tinyButton = document.getElementById('light');
 
-tinyButton.addEventListener('click', function(e){
+if(tinyButton) {
+  tinyButton.addEventListener('click', function(e){
        var nodes = tinyButton.children;
        for(var i = 0; i < nodes.length; i++){
            if(nodes[i].classList.contains("highlightbutton")){
@@ -10,31 +11,30 @@ tinyButton.addEventListener('click', function(e){
        if(e.target.id === "button1" || e.target.id === "button2" || e.target.id === "button3" || e.target.id === "button4" ){
            e.target.className = "highlightbutton";
      }
-});
+   })
+ };
 
 var colorButton = document.getElementById('colorGrid');
 
 colorButton.addEventListener('click', function(e){
        var nodes = colorButton.children;
        for(var i = 0; i < nodes.length; i++){
-           if(nodes[i].classList.contains("highlightcolorbutton[i]")) {
-               nodes[i].className = "";
-           }
+               nodes[i].className = "detailcolors"+(i+1);
        }
        if(e.target.id === "detailcolorselect1" || e.target.id === "detailcolorselect2" || e.target.id === "detailcolorselect3" || e.target.id === "detailcolorselect4" ){
-           e.target.className = "highlightcolorbutton[i]";
+           var x = e.target.id;
+           e.target.className = "highlightcolorbutton"+x[x.length-1];
      }
 });
 
 var cartText = document.getElementById('cartButton');
 var myCartCount = document.getElementById('cartCount');
-var incrementer = 0
+var qtyCount = parseInt(document.getElementById('qty').value, 1);
 
 function changeCartText(){
   if(cartText.innerHTML == 'Add to Cart') {
     cartText.innerHTML = "Added!";
-    incrementer++;
-    myCartCount.innerHTML = incrementer;
+    myCartCount.innerHTML = qtyCount;
   }
   else {
     cartText.innerHTML = "Add to Cart";
