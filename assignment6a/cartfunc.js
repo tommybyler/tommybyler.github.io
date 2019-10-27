7f48678e676b7b4c7a8e9b0e49451b67756f05e5
synchrxny <55989738+synchrxny@users.noreply.github.com>
2019-10-27 19:27:20 -0400

    folder name

--- assignment6a/cartfunc.js
+++
@@ -1,7 +0,0 @@
-var x = document.getElementById("card1");
-
-x.addEventListener('click', function(){
-  // alert("11111");
-    console.log(x.parentElement);
-    x.parentElement.parentElement.style.display = "none";
-});
