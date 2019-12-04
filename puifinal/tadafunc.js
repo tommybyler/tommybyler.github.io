7098a3eed62b5724ec3d0afd971600ef0164b28a
synchrxny <55989738+synchrxny@users.noreply.github.com>
2019-12-03 19:48:01 -0500

    updates

--- puifinal/tadafunc.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  $(".fancy-button").mousedown(function(){
-    $(this).bind('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd', function(){
-        $(this).removeClass('active');
-    })
-     $(this).addClass("active");
-  });
-});
